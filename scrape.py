8de09935e957059988887fc78d3b3ea1b4a40336
diksham1 <dikshamodi28@gmail.com>
2017-10-28 08:04:35 +0530

    Completed the bare basic script

--- scrape.py
+++ scrape.py
@@ -10,4 +10,15 @@ page = client.read()
 
 page = soup(page,'html.parser')
 
-container = page.find_all('div')
+container = page.find_all('a', {"class":"coin-list__body__row"})
+
+for item in container:
+    name = item.find('span',{"class":"coin-name"}).text
+    cur_price = item.find('span',{'class':"coin-list__body__row__price__value"}).text
+    market_cap = item.find('span',{'class':'coin-list__body__row__market-cap__value'}).text
+    percent_change = item.find('span',{'class':'coin-list__body__row__change'}).text.strip()
+
+    print "Name:\t\t" + name
+    print "Current Price:\t" + cur_price
+    print "Market Cap:\t" + market_cap
+    print "24 Hr change:\t" + percent_change + '\n'
